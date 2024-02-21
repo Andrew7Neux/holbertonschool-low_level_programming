eb74a64b67e7051e8f5cd7fd4ffcb35324735618
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-21 15:25:34 -0400

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+{
+	int abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	putchar(abc);
+	putchar('\n');
+	return (0);
+}
+
