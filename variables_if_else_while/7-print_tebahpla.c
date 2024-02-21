9e79ef01d40ba6e1da86d3eab905a01b106ef8b1
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-20 21:33:44 -0400

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char low;
-	for (low = 'z'; low >= 'a'; low--)
-			putchar(low);
-			putchar('\n');
+	int rev;
+
+	for (rev = 'z'; rev >= 'a'; rev--)
+			putchar(rev);
+
+					putchar('\n');
 
 		return (0);
 }
