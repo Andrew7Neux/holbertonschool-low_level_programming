1bab10e888a98a4d9a7faec065370169668622a5
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-21 16:34:31 -0400

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@
 
 void print_alphabet(void)
 {
-		int abc;
+	int abc;
 
-		for (abc = 'a'; abc <= 'z'; abc++)
-				_putchar(abc);
-		_putchar('\n');
+	for (abc = 'a'; abc <= 'z'; abc++)
+			_putchar(abc);
+	_putchar('\n');
 }
