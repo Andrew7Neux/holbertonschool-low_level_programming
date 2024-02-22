44496b4b3a93b473932420e11adf3d4065faa605
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-21 23:20:11 -0400

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - prints the lower case letters
+ *@c: variable
+ *Return: 1 if is lowercase else 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
