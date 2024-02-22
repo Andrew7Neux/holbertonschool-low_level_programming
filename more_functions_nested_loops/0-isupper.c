a2f3780904faecf8076c5da5de0c1ab0a8076f80
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-22 16:05:34 -0400

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -10,15 +10,16 @@
 int _isupper(int c)
 {
 	char uppercase = 'A';
+	
 	int isupper - 0;
 
 	for (; uppercase <= 'Z'; uppercase++)
 	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
+	if (c == uppercase)
+	{
+		isupper = 1;
+		break;
+	}
 	}
 	return (isupper);
 }
