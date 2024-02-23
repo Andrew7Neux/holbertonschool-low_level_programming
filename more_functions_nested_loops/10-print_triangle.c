3533c09ccf71c04b00f2e89b473cfddffb6feea7
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-23 00:01:11 -0400

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	for (lines = 1; lines <= size; lines++)
 	{
@@ -19,14 +19,14 @@ void print_triangle(int size)
 		{
 			if ((size - trg) >= lines)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
 			else
 			{
-				_putchar('#');
+				putchar('#');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 
 }
