e8eb7a1c6369f5316b0134295840424908fde7ca
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-22 23:12:33 -0400

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_most_numbers - print numbers exept 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+		i++;
+	}
+	putchar('\n');
+}
