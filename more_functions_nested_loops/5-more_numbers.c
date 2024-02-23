98c9fffab9fe429e6c66383c258afb8314408ee3
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-23 10:51:58 -0400

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * more_numbers - This prints a set of number from 0-14.
@@ -15,15 +14,15 @@ void more_numbers(void)
 	{
 		for (num = 0 ; num <= 14 ; num++)
 		{
-		if (num > 9)
-		{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
-		}
-		else
-		{
-		putchar(num + '0');
-		}
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			else
+			{
+				_putchar(num + '0');
+			}
 		}
 	}
 }
