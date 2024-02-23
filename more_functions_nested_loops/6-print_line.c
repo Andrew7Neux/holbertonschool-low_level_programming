70ea01e95f9c234d7266dcc03cd98d58f92eb8f2
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-23 00:10:53 -0400

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_line - funcion to print lines
+ *@n: number of times that will print lines
+ *
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			putchar('_');
+			i++;
+		}
+	}
+	putchar('\n');
+}
