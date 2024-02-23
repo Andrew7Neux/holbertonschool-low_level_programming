3ee528b78ac4cf1d0f785cac776706467436dd2d
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-22 23:51:27 -0400

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_diagonal - funcion to print diagonals
+ *@n: number of times that diagonal will print
+ */
+
+void print_diagonal(int n)
+{
+	int i, spc;
+
+	if (n >= 1)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (spc = 0 ; spc < i ; spc++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
