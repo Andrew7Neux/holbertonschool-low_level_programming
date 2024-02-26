98ef0069e49e339e89dfba8011f7557fe36dc633
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-26 15:02:20 -0400

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap integer values
+ * @a the first var to swap
+ * @b the second var to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
