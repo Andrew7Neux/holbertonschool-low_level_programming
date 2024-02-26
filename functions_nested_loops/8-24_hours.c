31bdf16dc1029505bc3f27d5280bf6a972c03b09
Andrew7Neux <jmartineztrade7@outlook.com>
2024-02-26 00:43:17 -0400

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *jack_bauer - run a clock of 24hr
+ */
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 00 ; hr <= 23 ; hr++)
+	{
+		for (min = 00 ; min <= 59 ; min++)
+		{
+
+			_putchar ((hr / 10) + '0');
+			_putchar ((hr % 10) + '0');
+			_putchar (':');
+			_putchar ((min / 10) + '0');
+			_putchar ((min % 10) + '0');
+			_putchar ('\n');
+		}
+	}
+}
